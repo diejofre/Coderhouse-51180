e3a7677a8c153693e400d6cb24b4d01c8108cc38
Diego <diego-jofre@live.com.ar>
2023-03-14 21:17:06 -0300

    add ProductItem component

--- 6/promises/src/App.jsx
+++ 6/promises/src/App.jsx
@@ -1,21 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import ProductItem from "./components/ProductItem";
+
+const URL_API = "https://fakestoreapi.com/products";
 
 function App() {
   const [productos, setProductos] = useState([]);
 
-  useEffect(() => {
-    axios("https://fakestoreapi.com/products").then((res) =>
-      setProductos(res.data)
-    );
-  }, []);
+  const getProductos = async () => {
+    try {
+      const res = await axios(URL_API);
+      setProductos(res.data);
+    } catch (error) {
+      console.log("ERROR: " + error);
+    }
+  };
 
-  console.log(productos);
+  useEffect(() => {
+    getProductos();
+    // axios("https://fakestoreapi.com/products").then((res) =>
+    //   setProductos(res.data)
+    // ).catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
       <h1>React App</h1>
+      {productos.map((producto, i) => (
+        <ProductItem producto={producto} key={producto.id} />
+      ))}
     </div>
   );
 }
